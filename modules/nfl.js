3df4d2e3058a43ba055079cf34c8c507ccb9a3fb
05knozrese <150184441+05knozrese@users.noreply.github.com>
2026-06-24 10:35:51 -0400

    Refactor NFL fetch and command responses

--- modules/nfl.js
+++ modules/nfl.js
@@ -1,17 +1,20 @@
 let client;
 let channel = null;
-let lastMessage = null;
 
 async function fetchNFL() {
-  const res = await fetch("https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard");
-  const data = await res.json();
+  try {
+    const res = await fetch("https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard");
+    const data = await res.json();
 
-  return data.events.slice(0, 5).map(g => {
-    const c = g.competitions[0];
-    const t = c.competitors;
+    return data.events.slice(0, 5).map(g => {
+      const c = g.competitions[0];
+      const t = c.competitors;
 
-    return `🏈 ${t[0].team.abbreviation} ${t[0].score} - ${t[1].score} ${t[1].team.abbreviation} | ${c.status.type.shortDetail}`;
-  }).join("\n");
+      return `🏈 ${t[0].team.abbreviation} ${t[0].score} - ${t[1].score} ${t[1].team.abbreviation}`;
+    }).join("\n");
+  } catch {
+    return "NFL error";
+  }
 }
 
 function init(c) {
@@ -22,14 +25,7 @@ function init(c) {
 
     try {
       const ch = await client.channels.fetch(channel);
-      const msg = await fetchNFL();
-
-      // prevent spam
-      if (msg === lastMessage) return;
-      lastMessage = msg;
-
-      await ch.send("📊 **NFL LIVE UPDATE**\n" + msg);
-
+      ch.send("📊 NFL LIVE\n" + await fetchNFL());
     } catch {}
   }, 30000);
 }
@@ -37,7 +33,7 @@ function init(c) {
 function commands(client, m) {
   if (m.content === "!nfl on") {
     channel = m.channel.id;
-    m.reply("🏈 NFL LIVE DASHBOARD ON");
+    m.reply("🏈 NFL ON");
   }
 
   if (m.content === "!nfl off") {
@@ -46,7 +42,7 @@ function commands(client, m) {
   }
 
   if (m.content === "!nfl") {
-    fetchNFL().then(x => m.reply("📊 NFL:\n" + x));
+    fetchNFL().then(x => m.reply(x));
   }
 }
 
