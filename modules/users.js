c944bbd909032de9ec216b3b81a8397116f4d918
05knozrese <150184441+05knozrese@users.noreply.github.com>
2026-06-24 10:22:45 -0400

    Update users.js

--- modules/users.js
+++ modules/users.js
@@ -1,1 +1,25 @@
+const settings = {};
 
+function commands(client, m) {
+  const id = m.author.id;
+
+  if (!settings[id]) {
+    settings[id] = { ping: true };
+  }
+
+  if (m.content === "!ping on") {
+    settings[id].ping = true;
+    return m.reply("🔔 Ping ON");
+  }
+
+  if (m.content === "!ping off") {
+    settings[id].ping = false;
+    return m.reply("🔕 Ping OFF");
+  }
+
+  if (m.content === "!me") {
+    return m.reply(`Ping mode: ${settings[id].ping}`);
+  }
+}
+
+module.exports = { commands };
