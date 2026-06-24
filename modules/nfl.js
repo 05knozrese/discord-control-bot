620523f8c15ad701c97040628fa088f575d69283
05knozrese <150184441+05knozrese@users.noreply.github.com>
2026-06-24 10:22:14 -0400

    Implement NFL live score fetching and commands

--- modules/nfl.js
+++ modules/nfl.js
@@ -1,1 +1,44 @@
+let client;
+let channel = null;
 
+async function fetchNFL() {
+  const res = await fetch("https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard");
+  const data = await res.json();
+
+  return data.events.slice(0, 5).map(g => {
+    const c = g.competitions[0];
+    const t = c.competitors;
+
+    return `🏈 ${t[0].team.abbreviation} ${t[0].score} - ${t[1].score} ${t[1].team.abbreviation}
+⏱ ${c.status.type.shortDetail}`;
+  }).join("\n");
+}
+
+function init(c) {
+  client = c;
+
+  setInterval(async () => {
+    if (!channel) return;
+
+    const ch = await client.channels.fetch(channel);
+    ch.send(await fetchNFL());
+  }, 30000);
+}
+
+function commands(client, m) {
+  if (m.content === "!nfl on") {
+    channel = m.channel.id;
+    m.reply("🏈 NFL LIVE ENABLED");
+  }
+
+  if (m.content === "!nfl off") {
+    channel = null;
+    m.reply("🏈 NFL OFF");
+  }
+
+  if (m.content === "!nfl") {
+    fetchNFL().then(x => m.reply(x));
+  }
+}
+
+module.exports = { init, commands };
