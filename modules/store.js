23aec8f946f36941d087bf0db068c2aafa8b99ce
05knozrese <150184441+05knozrese@users.noreply.github.com>
2026-06-24 10:23:09 -0400

    Update store.js

--- modules/store.js
+++ modules/store.js
@@ -1,1 +1,15 @@
+const fs = require("fs");
 
+function save(name, data) {
+  fs.writeFileSync(`./${name}.json`, JSON.stringify(data));
+}
+
+function load(name) {
+  try {
+    return JSON.parse(fs.readFileSync(`./${name}.json`));
+  } catch {
+    return null;
+  }
+}
+
+module.exports = { save, load };
